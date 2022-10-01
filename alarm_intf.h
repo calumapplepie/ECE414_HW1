fecb287037e70a69a548283990560bc0993e35f5
Calum McConnell <calumlikesapplepie@gmail.com>
2022-10-01 13:48:37 -0400

    more provided header file improvements

--- alarm_intf.h
+++ alarm_intf.h
@@ -1,15 +1,23 @@
 #ifndef ALARM_INTF_H
 #define ALARM__INTF_H
+
 #include <inttypes.h>
 // Initialize the alarm interfaces
 void initAlarm();
+
+// gets the status of the door: 0 for closed, 1 for open
 uint8_t readDoor();
+
 // turn warning LED OFF
 void ledOff();
+
 // turn warning LED ON
 void ledOn();
+
 // turn alarm buzzer OFF
 void alarmOff();
+
 // turn alarm buzzer ON
 void alarmOn();
+
 #endif
